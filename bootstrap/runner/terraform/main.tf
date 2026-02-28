83626999673977eeeea601226861f4a846b26b2f
AkihisaMaruyama <akihisa58591371@gmail.com>
2026-03-01 03:24:05 +0900

    chore

--- bootstrap/runner/terraform/main.tf
+++ bootstrap/runner/terraform/main.tf
@@ -1,6 +1,6 @@
 provider "proxmox" {
   pm_api_url = var.pve_api_url
-  pm.api_token_id = var.pve_api_token_id
+  pm_api_token_id = var.pve_api_token_id
   pm_api_token_secret = var.pve_api_token_secret
   pm_tls_insecure = true
 }
